4f858c6ea26284815db639ac8351d0f99e0ee998
jonathanbraga47 <jonathanbraga.dev@gmail.com>
2026-08-05 16:54:19 -0300

    refactor: transfere logica de login e logout do AuthController para o AuthService

---
+++ backend/src/main/java/PlataformaIdeiasInovacao/auth/AuthController.java
@@ -0,0 +1,35 @@
+package PlataformaIdeiasInovacao.auth;
+
+import PlataformaIdeiasInovacao.auth.dto.AuthenticationDTO;
+import PlataformaIdeiasInovacao.auth.dto.LoginResponseDTO;
+import PlataformaIdeiasInovacao.auth.dto.RegisterDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterDTO data) {
+        this.authService.register(data);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO data) {
+        LoginResponseDTO response = this.authService.login(data);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        this.authService.logout();
+        return ResponseEntity.noContent().build();
+    }
+}
