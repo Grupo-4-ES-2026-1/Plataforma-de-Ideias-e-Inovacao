7a8fa32d3d1c5b9893887c5cf3d06e5b5d20ece4
Pedru Doarte <pedrodsd51@gmail.com>
2026-08-11 16:48:41 -0300

    Preenchendo DTOs de entrada e saida de Proposta

--- backend/src/main/java/PlataformaIdeiasInovacao/proposta/dto/PropostaRequestDTO.java
+++ backend/src/main/java/PlataformaIdeiasInovacao/proposta/dto/PropostaRequestDTO.java
@@ -1,4 +1,39 @@
 package PlataformaIdeiasInovacao.proposta.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class PropostaRequestDTO {
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String descricao;
+
+    @NotBlank
+    private String categoria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
