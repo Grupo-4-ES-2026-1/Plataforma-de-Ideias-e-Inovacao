b3739c55369be5c7879d68e47856312c57d4ba6c
jonathanbraga47 <jonathanbraga.dev@gmail.com>
2026-08-10 23:42:56 -0300

    feat: Cria endpoint de registro de Proposta

--- backend/src/main/java/PlataformaIdeiasInovacao/proposta/PropostaController.java
+++ backend/src/main/java/PlataformaIdeiasInovacao/proposta/PropostaController.java
@@ -1,7 +1,12 @@
 package PlataformaIdeiasInovacao.proposta;
 
 
+import PlataformaIdeiasInovacao.auth.dto.RegisterDTO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PropostaController {
 
     @Autowired
-    private PropostaService propostaService;
+   private PropostaService propostaService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid PropostaRequestDTO data){
+        this.propostaService.register(data);
+        return ResponseEntity.ok().build();
+    }
 }
