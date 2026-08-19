275cbde38a47e1650c3c4c2f487304a4ba0431fb
Pedru Doarte <pedrodsd51@gmail.com>
2026-08-19 12:54:36 -0300

    Preenchendo VotoController.java

--- backend/src/main/java/PlataformaIdeiasInovacao/voto/VotoController.java
+++ backend/src/main/java/PlataformaIdeiasInovacao/voto/VotoController.java
@@ -1,5 +1,27 @@
 package PlataformaIdeiasInovacao.voto;
 
+import PlataformaIdeiasInovacao.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/propostas")
 public class VotoController {
 
+    @Autowired
+    private VotoService votoService;
+
+    @PostMapping("/{propostaId}/voto")
+    public ResponseEntity<Voto> votar(
+            @PathVariable Long propostaId,
+            Authentication authentication) {
+
+        User usuario = (User) authentication.getPrincipal();
+
+        Voto voto = votoService.votar(propostaId, usuario);
+
+        return ResponseEntity.status(201).body(voto);
+    }
 }
