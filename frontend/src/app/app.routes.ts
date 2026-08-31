7abebf946ccf3f441ee6f45bd350fc4bf51bb0da
joyeuxpierrot <mario.ramon@ufape.edu.br>
2026-08-31 14:16:30 -0300

    feat: cria tela de dashboard com card de total e distribuicao (Resolves #141)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { PropostasListaComponent } from './features/propostas-lista/propostas-li
 import { PropostaDetalheComponent } from './features/proposta-detalhe/proposta-detalhe';
 import { PropostaCadastroComponent } from './features/cadastro-proposta/cadastro-proposta';
 import { MinhasPropostasComponent } from './features/minhas-propostas/minhas-propostas';
+import { DashboardComponent } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
   {
@@ -32,11 +33,11 @@ export const routes: Routes = [
     component: PropostasListaComponent,
     canActivate: [authGuard]
   },
-{
+  {
   path: 'minhas-propostas',
   component: MinhasPropostasComponent,
   canActivate: [authGuard]
-},
+  },
   {
     path: 'propostas/nova',
     component: PropostaCadastroComponent,
@@ -45,6 +46,11 @@ export const routes: Routes = [
   {
     path: 'propostas/:id',
     component: PropostaDetalheComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
     canActivate: [authGuard]
   }
 ];
